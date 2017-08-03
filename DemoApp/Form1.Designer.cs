665700ec4f729f55487fec3dde50cef26d4ca33f
林选臣 <xuanchenlin@msn.com>
2017-08-03 21:35:28 +0800

    CREATE Project

---
+++ DemoApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DemoApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.BackgroundImage = global::DemoApp.Properties.Resources.MeckEd_PayPal_donate_button;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Location = new System.Drawing.Point(131, 325);
+			this.button1.Name = "button1";
+			this.button1.Padding = new System.Windows.Forms.Padding(10);
+			this.button1.Size = new System.Drawing.Size(203, 104);
+			this.button1.TabIndex = 1;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.BackgroundImage = global::DemoApp.Properties.Resources.beg_with_border;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.Location = new System.Drawing.Point(9, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(446, 300);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.ActiveBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(50)))), ((int)(((byte)(138)))));
+			this.ActiveShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(95)))), ((int)(((byte)(197)))));
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 441);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.InactiveShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(95)))), ((int)(((byte)(197)))));
+			this.Location = new System.Drawing.Point(0, 0);
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ModernUI Demo Application";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,35 @@
+# ModernUI Theme for .Net WinForms
+
+**ModernUI Theme** is a skin library that makes your .Net Forms look like ModernUI window in Win8/8.1 or Win10.
+
+It redrew borders of stardard WinForm, and make a dropshadow around the window. You can set your form's border size and color, also you can set color of the dropshadow too.
+
+
+
+# Features
+- Make ModernUI-like Forms for .Net Windows Form Applications.
+- Full window animations support (Not just set FormBorderStyle to None).
+- Fast draw the dropshadow around the form.
+- Support Form Active/Inactive state.
+
+# Example
+
+
+```C#
+	public partial class Form1 : ModernUIForm
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+```
+
+Change properties in "UI" category to make your form style as your wish.
+
+# Donate
+
+If you like my work, please buy me a cup of coffee to encourage me continue with this library. 
+![Screen Shot](http://www.ohtrip.cn/media/beg_with_border.png)
+
+[![DONATE](http://www.ohtrip.cn/media/PayPal-donate-button.png)](https://www.paypal.me/mrjson)
